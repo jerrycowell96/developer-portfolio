19e99c8d5d977f33ecd10842a4728dcabcda7f7b
Jerry Cowell <jerrycowell96@gmail.com~>
2023-11-01 12:52:12 -0400

    add work files

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Work.css"
+import Works from './Works';
+
+const Work = () => {
+  return (
+    <section className="work container section">
+    <h2 className="section__title"> My recent works</h2>
+    <span className="section__subtitle">portfolio</span>
+
+    <Works /> 
+    </section>
+  )
+}
+
+export default Work
+
+
+
+
